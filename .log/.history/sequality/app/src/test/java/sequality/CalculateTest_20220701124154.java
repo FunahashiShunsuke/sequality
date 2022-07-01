b61d241eef8ba3ef98b1241bca37d45be88931fa
FunahashiShunsuke <e1q19073@st.oit.ac.jp>
2022-07-01 12:45:20 +0900

    add Calculate

---
+++ .log/.history/sequality/app/src/test/java/sequality/CalculateTest_20220701124154.java
@@ -0,0 +1,10 @@
+package sequality;
+
+public class CalculateTest {
+  @Test
+  public void testSum() {
+    Calculate calculate = new Calculate();
+    int expected = 5;
+    assertEquals(expected, calculate.sum(2, 3));
+  }
+}
